9ccb3663197912bf39c2aeea9b917927e22e841d
Touhidhul Islam Jisan <touhidulislamjisan@gmail.com>
2020-10-04 01:16:31 +0600

    Pet Clinic Initial Commit

---
+++ pet-clinic/src/test/java/com/touhid/petclinic/PetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.touhid.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
